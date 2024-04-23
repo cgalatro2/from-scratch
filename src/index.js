a7dfc7367ce166250b4bab14fd7248717508d477
Chase Galatro <chasegalatro@chases-air.mynetworksettings.com>
2024-04-23 15:40:06 -0400

    app now resolves jsx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App";
+
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
-root.render(<h1>Sup React</h1>);
+root.render(<App />);
